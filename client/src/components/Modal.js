be9534305b80fcd0f6af0fec4a01d39a334e1014
ericstober <erictstober@gmail.com>
2024-04-05 10:51:34 -0500

    added front end html and css, added modal component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,28 @@
+class Modal {
+  constructor() {
+    this._modal = document.querySelector('#modal');
+    this._modalBtn = document.querySelector('#modal-btn');
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this._modalBtn.addEventListener('click', this.openModal.bind(this));
+    window.addEventListener('click', this.outsideClick.bind(this));
+  }
+
+  openModal() {
+    this._modal.style.display = 'block';
+  }
+
+  closeModal() {
+    this._modal.style.display = 'none';
+  }
+
+  outsideClick(e) {
+    if (e.target === this._modal) {
+      this.closeModal();
+    }
+  }
+}
+
+export default Modal;
